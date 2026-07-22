68f3b967395164b32768be2b717ce998bece5461
trungtin87 <trungtincatp@gmail.com>
2026-07-22 19:04:53 +0700

    remove

--- src/config.ts
+++
@@ -1,7 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Huyền Không Thư Quán';
-export const SITE_DESCRIPTION = 'Blog cá nhân theo tinh thần Đạo giáo hiện đại: đơn giản, tự nhiên, không dư thừa. Ghi chép kỹ thuật, triết học và các dự án của Bùi Trung Tín.';
-export const GENERATE_SLUG_FROM_TITLE = true
-export const TRANSITION_API = true
